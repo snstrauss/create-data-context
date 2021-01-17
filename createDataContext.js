99ad08cd166c6138ae90fa5482d6d66259e5b3a6
snstrauss <zahavi.guy.85@gmail.com>
2021-01-17 09:29:08 +0200

    Update createDataContext.js

--- createDataContext.js
+++ createDataContext.js
@@ -1,5 +1,42 @@
 import React, { createContext, useMemo, useReducer } from 'react';
 
+/**
+*
+*   USAGE
+*
+*   
+import createDataContext from './createDataContext';
+
+const locationActions = {
+    start: (state) => ({
+        ...state,
+        recording: true
+    }),
+    stop: (state) => ({
+        ...state,
+        recording: false
+    }),
+    add: (state, newLocation) => ({
+        ...state,
+        currentLocation: newLocation.coords,
+        locations: [...state.locations, newLocation]
+    })
+};
+
+const { Provider, Context } = createDataContext('location', locationActions, {
+    recording: false,
+    locations: [],
+    currentLocation: null
+});
+
+const LocationProvider = Provider;
+
+export const LocationContext = Context;
+export default LocationProvider;
+*
+*
+*
+*/
 export default function createDataContext(contextName, actions, initialState){
 
     /**
